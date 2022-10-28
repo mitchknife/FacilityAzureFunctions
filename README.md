670de97064b6cf93876b420ad786d87129221afe
Mitch Rosenburg <mitch@inksoft.com>
2022-10-28 12:30:08 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# FacilityAzureFunctions
+
+Repository description.
+
+[![Build](https://github.com/FacilityApi/FacilityAzureFunctions/workflows/Build/badge.svg)](https://github.com/FacilityApi/FacilityAzureFunctions/actions?query=workflow%3ABuild)
+
+[Documentation](https://facilityapi.github.io/) | [Release Notes](ReleaseNotes.md) | [Contributing](CONTRIBUTING.md)
---
+++ tools/Build/Build.cs
@@ -0,0 +1,49 @@
+return BuildRunner.Execute(args, build =>
+{
+	var codegen = "fsdgen___";
+
+	var gitLogin = new GitLoginInfo("FacilityApiBot", Environment.GetEnvironmentVariable("BUILD_BOT_PASSWORD") ?? "");
+
+	var dotNetBuildSettings = new DotNetBuildSettings
+	{
+		NuGetApiKey = Environment.GetEnvironmentVariable("NUGET_API_KEY"),
+		DocsSettings = new DotNetDocsSettings
+		{
+			GitLogin = gitLogin,
+			GitAuthor = new GitAuthorInfo("FacilityApiBot", "facilityapi@gmail.com"),
+			SourceCodeUrl = "https://github.com/FacilityApi/FacilityAzureFunctions/tree/master/src",
+			ProjectHasDocs = name => !name.StartsWith("fsdgen", StringComparison.Ordinal),
+		},
+		PackageSettings = new DotNetPackageSettings
+		{
+			GitLogin = gitLogin,
+			PushTagOnPublish = x => $"nuget.{x.Version}",
+		},
+	};
+
+	build.AddDotNetTargets(dotNetBuildSettings);
+
+	build.Target("codegen")
+		.DependsOn("build")
+		.Describe("Generates code from the FSD")
+		.Does(() => CodeGen(verify: false));
+
+	build.Target("verify-codegen")
+		.DependsOn("build")
+		.Describe("Ensures the generated code is up-to-date")
+		.Does(() => CodeGen(verify: true));
+
+	build.Target("test")
+		.DependsOn("verify-codegen");
+
+	void CodeGen(bool verify)
+	{
+		var configuration = dotNetBuildSettings.GetConfiguration();
+		var verifyOption = verify ? "--verify" : null;
+
+		RunCodeGen("___", "___");
+
+		void RunCodeGen(params string?[] args) =>
+			RunDotNet(new[] { "run", "--no-build", "--project", $"src/{codegen}", "-f", "net6.0", "-c", configuration, "--", "--newline", "lf", verifyOption }.Concat(args));
+	}
+});
